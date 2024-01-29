96cb6f3b863790ee384d7a02b77b8a841696ac67
Marcelo Augusto <mrcelo014@gmail.com>
2024-01-29 10:41:58 -0300

    Recovering from Errors

--- src/app/products/[productID]/page.tsx
+++ src/app/products/[productID]/page.tsx
@@ -1,23 +1,44 @@
-import { notFound } from "next/navigation";
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
+interface ParamsProps {
+  params: {
+    productID: string
+    reviewsID: string
+  }
+}
+
+export const generateMetadata = async ({
+  params
+}: ParamsProps): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone reviwes ${params.reviewsID}`)
+    }, 100)
+  })
+
+  return {
+    title: `Title ${title}`
+  }
+}
 export default function ProductsDetails({
-    params,
+  params
 }: {
-    params: { productID: string };
+  params: { productID: string }
 }) {
-    if (parseInt(params.productID) > 100) {
-        notFound();
-    }
-    return (
-        <>
-            <h1>{params.productID}</h1>
-            <p>
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                dolorem dolorum veniam repudiandae, sed cumque ab accusantium
-                sit hic itaque recusandae corporis corrupti quas sunt quasi
-                dignissimos labore modi? Qui.
-            </p>
-        </>
-    );
+  if (parseInt(params.productID) > 100) {
+    notFound()
+  }
+  return (
+    <>
+      <h1>{params.productID}</h1>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem
+        dolorum veniam repudiandae, sed cumque ab accusantium sit hic itaque
+        recusandae corporis corrupti quas sunt quasi dignissimos labore modi?
+        Qui.
+      </p>
+    </>
+  )
 }
--- src/app/products/[productID]/reviews/[reviewsID]/page.tsx
+++ src/app/products/[productID]/reviews/[reviewsID]/page.tsx
@@ -1,36 +1,32 @@
-import { notFound } from "next/navigation";
-import { Metadata } from "next";
+'use client'
+
+import { notFound } from 'next/navigation'
 
 interface ParamsProps {
-    params: {
-        productID: string;
-        reviewsID: string;
-    };
+  params: {
+    productID: string
+    reviewsID: string
+  }
 }
 
-export const generateMetadata = async ({
-    params,
-}: ParamsProps): Promise<Metadata> => {
-    const title = await new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(`Iphone reviwes ${params.reviewsID}`);
-        }, 100);
-    });
-
-    return {
-        title: `Title ${title}`,
-    };
-};
+const getReandomNumber = (num: number) => {
+  return Math.floor(Math.random() * num)
+}
 
 export default function ProductIdReview({ params }: ParamsProps) {
-    if (parseInt(params.reviewsID) > 100) {
-        notFound();
-    }
-    return (
-        <h1>
-            Product id: <strong>{params.productID}</strong>
-            <br />
-            ProductReviews: <strong>{params.reviewsID}</strong>
-        </h1>
-    );
+  const random = getReandomNumber(2)
+
+  if (random === 0) {
+    throw new Error('Error loading product review')
+  }
+  if (parseInt(params.reviewsID) > 100) {
+    notFound()
+  }
+  return (
+    <h1>
+      Product id: <strong>{params.productID}</strong>
+      <br />
+      ProductReviews: <strong>{params.reviewsID}</strong>
+    </h1>
+  )
 }
